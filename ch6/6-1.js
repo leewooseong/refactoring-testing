2af3907143959c07704441db1a3f64ecc276df07
leewooseong <sitpo96@naver.com>
2022-07-11 19:33:59 +0900

    feat: 함수 추출하기 리팩토링 코드 작성

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,31 +1,60 @@
+// printOwing 함수 내부에서 사용되는 함수들은 export 되지 않아서 함수 외부에서는 사용이 불가능하다.
 export function printOwing(invoice) {
-  let outstanding = 0;
+    // 배너 출력
+    printBanner();
 
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
+    // 총 가격 계산
+    let outstanding = calculateOutstanding(invoice);
 
-  // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
+    // 지급 날짜를 계산
+    recordDueDate(invoice);
 
-  // record due date
-  const today = new Date();
-  invoice.dueDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() + 30
-  );
+    //세부 사항 출력
+    printDetails(invoice, outstanding);
+}
 
-  //print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+function printBanner() {
+    console.log("***********************");
+    console.log("**** Customer Owes ****");
+    console.log("***********************");
+}
+
+function calculateOutstanding(invoice) {
+    // 함수 내에서 결과값을 반환하는 목적의 변수라면 result라는 이름을 많이 사용
+    // outstanding -> result 변수면 변경
+
+    // 반복문(절차지향적 코드) 작성 -> 파이프라인(함수형프로그래밍)으로 변경
+    // 1. 반복문
+    // let result = 0;
+    // for (const o of invoice.orders) {
+    //     result += o.amount;
+    // }
+    //
+    // return result;
+    // 2. 파이프라인
+    return invoice.orders.reduce((sum, order) => (sum += order.amount), 0);
+}
+
+// 함수 내부에서 객체의 내용이 변경되기 때문에 함수의 이름에 이런 내용도 녺여내야한다.
+function recordDueDate(invoice) {
+    const today = new Date();
+    invoice.dueDate = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate() + 30
+    );
+    // ※ invoice 객체 수정
+    // - invoice에 dueDate라는 속성을 추가해준다. -> 불변성 참고
+}
+
+function printDetails(invoice, outstanding) {
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
 }
 
 const invoice = {
-  orders: [{ amount: 2 }, { amount: 5 }],
-  customer: '엘리',
+    orders: [{ amount: 2 }, { amount: 5 }],
+    customer: "엘리",
 };
 printOwing(invoice);
--- ch6_my/6-1.js
+++ ch6_my/6-1.js
@@ -1,24 +1,34 @@
-export function printOwing(invoice) {
-    let outstanding = 0;
-
-    console.log("***********************");
-    console.log("**** Customer Owes ****");
-    console.log("***********************");
-
-    // calculate outstanding
-    for (const order of invoice.orders) {
+function calculateOutstanding(outstanding, orderList) {
+    for (const order of orderList) {
         outstanding += order.amount;
     }
+    return outstanding;
+}
 
-    // record due date
+function recordDueDate() {
     const today = new Date();
-    invoice.dueDate = new Date(
+    return new Date(
         today.getFullYear(),
         today.getMonth(),
         today.getDate() + 30
     );
+}
+
+// 청구서 출력함수
+// invoice: 청구서, 송장
+// owe: 빚
+// outstanding: 미지불된 빚?
+// dueDate: 지불예정일
+export function printOwing(invoice) {
+    let outstanding = 0;
+    outstanding = calculateOutstanding(outstanding, invoice.orders);
+    invoice.dueDate = recordDueDate();
 
     //print details
+    console.log("***********************");
+    console.log("**** Customer Owes ****");
+    console.log("***********************");
+
     console.log(`name: ${invoice.customer}`);
     console.log(`amount: ${outstanding}`);
     console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
